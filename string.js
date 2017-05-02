70e32d5bb291ec6c3577c43d0f92b5977ee406b3
amazzenga <7amazzenga@stpatrick.org>
2017-05-02 13:55:47 -0500

    worked on string.js

--- string.js
+++ string.js
@@ -41,22 +41,75 @@ exports.containsUpper =function(str){
         for(var a=0;a<str.length;a++){
             if(inRange(str[a],65,90)){
                 hasUpper = true;
+                throw PasswordMessage("Password has uppercase letter");
             }
         }
+        if(hasUpper = false){
+            throw PasswordMessage("Password does not have an uppercase letter");
+        }
+    }
+    catch(e){
+        console.log(e.message);
+        return hasUpper;
     }
 }
 
 
 exports.containsLower =function(str){
-
+    let hasLower = false;
+    try{
+        for(var a=0;a<str.length;a++){
+            if(inRange(str[a],97,122)){
+                hasLower = true;
+                throw PasswordMessage("Password has lowercase letter");
+            }
+        }
+        if(hasLower = false){
+            throw PasswordMessage("Password does not have an lowercase letter")
+        }
+    }
+    catch(e){
+        console.log(e.message);
+        return hasLower;
+    }
 }
 
-
 exports.containsNumerical =function(str){
-
+    let hasNumber = false;
+    try{
+        for(var a=0;a<str.length;a++){
+            if(inRange(str[a],48,57)){
+                hasNumber = true;
+                throw PasswordMessage("Password has a number");
+            }
+        }
+        if(hasNumber = false){
+            throw PasswordMessage("Password does not have a number");
+        }
+    }
+    catch(e){
+        console.log(e.message);
+        return hasNumber;
+    }
 }
 
 
 exports.containsSpecial =function(str){
-
+    let hasSpecial = false;
+    let special = [33,64,35,36,37,94,38,42];
+    try{
+        for(var a=0;a<str.length;a++){
+            for(var b=0;b<special.length,b++){
+                if(str[a]==special[b]){
+                    hasSpecial = true;
+                    throw PasswordMessage("Password has a special character")
+                }
+                if
+            }
+        }
+    }
+    catch(e){
+        console.log(e.message);
+        return hasSpecial;
+    }
 }
